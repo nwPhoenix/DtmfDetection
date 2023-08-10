7492f4153af0238a69806b216e48b69fe67cfb7d
nwPhoenix <104714027+nwPhoenix@users.noreply.github.com>
2023-08-10 19:42:48 +1000

    bruh

--- example/dtmf-detector/Program.cs
+++ example/dtmf-detector/Program.cs
@@ -9,7 +9,7 @@
     using NAudio.CoreAudioApi;
 
     public static class Program {
-        public static int Main(string[] args) {
+        public static int Main(string[] args) { 
             try {
                 PrintHeader();
 
